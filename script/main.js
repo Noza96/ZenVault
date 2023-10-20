b0420bc19c2abb7c62babc5a2b12caab988b2519
Noza96 <tornike.nozadze96@outlook.com>
2023-10-20 05:47:29 +0400

    Updated JS

--- script/main.js
+++ script/main.js
@@ -1,4 +1,4 @@
-let addTransactionVendor = document.getElementById('addTransactionVendor')
+let addTransactionVendor = document.getElementById('addTransactionVendorInput')
 let addTransactionPrice = document.getElementById('addTransactionPrice')
 let addTransactionType = document.getElementById('addTransactionType')
 let addTransactionDate = document.getElementById('addTransactionDate')
@@ -9,6 +9,7 @@ let addTransaction = document.getElementById('addTransaction')
 
 
 function transactionCloseFunc(){
+    let transactionWrapper = document.querySelector('.transactionWrapper');
     transactionWrapper.style.display = "none"
     transactionWrapper.innerHTML = ''; // Content is cleared
 }
@@ -41,7 +42,19 @@ function storeTransactionData(){
         'currency': addTransactionCurrency.value,
     }
    localStorage.setItem(localStorage.length + 1, JSON.stringify(userInput))
+   console.log("it works");
 }
+
+
+
+
+
+function determineType(type, price, arg){
+    if(type == arg){
+        return `${price}`
+    }else{return `${-price}`}
+}
+
 
 Object.keys(localStorage).forEach(key => {
     // if key is anything else but ID don't use it
@@ -51,7 +64,7 @@ Object.keys(localStorage).forEach(key => {
         let getTransactionDate = JSON.parse(localStorage.getItem(key)).date;
         let addTransactionType = JSON.parse(localStorage.getItem(key)).type;
         let addTransactionCurrency = JSON.parse(localStorage.getItem(key)).currency;
-    
+        
         var transactionItems = document.getElementById('transactionItems');
         var transactionItem = document.createElement("li")
         var transactionItemType = document.createElement('p')
@@ -61,7 +74,7 @@ Object.keys(localStorage).forEach(key => {
         transactionItem.setAttribute("onclick", `reply_click(${transactionItem.id})`)
 
         transactionItemType.textContent = getTransactionVendor;
-        transactionItemAmount.textContent = getTransactionPrice +" " + addTransactionCurrency;
+        transactionItemAmount.textContent = `${determineType(addTransactionType, getTransactionPrice, "შემოსავალი")}  ${addTransactionCurrency}`
         
         transactionItem.appendChild(transactionItemType)
         transactionItem.appendChild(transactionItemAmount)
@@ -76,95 +89,26 @@ Object.keys(localStorage).forEach(key => {
 function reply_click(clicked_id){
     transactionAmount = JSON.parse(localStorage.getItem(clicked_id)).amount;
     transactionVendor = JSON.parse(localStorage.getItem(clicked_id)).vendor
-    transactiondate = JSON.parse(localStorage.getItem(clicked_id)).date
+    transactionDate = JSON.parse(localStorage.getItem(clicked_id)).date
     transactionid = JSON.parse(localStorage.getItem(clicked_id)).id;
     transactionType = JSON.parse(localStorage.getItem(clicked_id)).type;
     transactionCurrency = JSON.parse(localStorage.getItem(clicked_id)).currency;
+ // itemAmount.textContent = 
 
     // console.log(clicked_id);
     document.getElementById(clicked_id).style.display = "grid"
+    let transactionWrapper = document.querySelector('.transactionWrapper')
+    let transactionItemID = document.querySelector('.transactionItemID')
 
-    //create transaction content
-    let transactionWrapper = document.getElementById('transactionWrapper')
-
-    let transactionArticle = document.createElement('article')
-    // create Header
-    let transactionHeader = document.createElement('div')
-    let transactionHeaderID = document.createElement('h3')
-    transactionHeaderID.textContent = "Transaction #" + clicked_id;
-
-    let transactionClose = document.createElement('button')
-    transactionClose.textContent = "X"
-    transactionClose.setAttribute("id", 'transactionClose')
-    transactionClose.setAttribute("onclick", 'transactionCloseFunc()')
-
-    transactionHeader.appendChild(transactionHeaderID)
-    transactionHeader.appendChild(transactionClose)
-    transactionArticle.appendChild(transactionHeader)
-    transactionWrapper.appendChild(transactionArticle)
-
-    // Create Body
-    let transactionBody = document.createElement('div')
-    let transactionBodyUl = document.createElement('ul')
-
-    // transaction type label
-    let transactionTypeLabel = document.createElement('label');
-    transactionTypeLabel.setAttribute("for", `transactionType`)
-    transactionTypeLabel.setAttribute("class", `transactionTypeLabel`)
-    transactionTypeLabel.textContent = transactionType;
-
-    // Date 
-    let transactionDate = document.createElement('li')
-    transactionDate.setAttribute('id', 'transactionType');
-    transactionDate.setAttribute('class', 'transactionTypeLabel');
-    transactionDate.textContent = transactiondate 
+    let itemType = document.querySelector('.itemType')
+    let itemAmount = document.querySelector('.itemAmount')
+    let itemDesc = document.querySelector('.itemDesc')
     
-    // Amount
-    let transactionAmountLabel = document.createElement('li')
-    transactionAmountLabel.setAttribute('class', ' amount')
-    transactionAmountLabel.textContent = `${transactionAmount} ${transactionCurrency} `
-    // Info
-    let transactionInfo = document.createElement('li')
-    transactionInfo.setAttribute('class', 'transactionInfo')
-        //label
-    let transactionInfoLabel = document.createElement('label')
-    transactionInfoLabel.setAttribute('class', 'transactionDescLabel')
-    transactionInfoLabel.setAttribute('for', 'transactionDesc')
-    transactionInfoLabel.textContent = 'აღწერა'
-    let transactionDesc = document.createElement('p')
-    transactionDesc.setAttribute('id', 'transactionDesc')
-    transactionDesc.textContent = `${transactionType} / თანხა ${transactionCurrency} ${transactionAmount} / ${transactiondate}`
-    transactionInfo.appendChild(transactionInfoLabel)
-    transactionInfo.appendChild(transactionDesc)
-
-    // Amount
-    let transactionInfoAmount = document.createElement('li')
-    transactionInfoAmount.setAttribute('class', 'transactionInfo')
-        //label
-    let transactionInfoAmountLabel = document.createElement('label')
-    transactionInfoAmountLabel.setAttribute('class', 'transactionAmountLabel transactionInfo')
-    transactionInfoAmountLabel.setAttribute('for', 'transactionAmount')
-    transactionInfoAmountLabel.textContent = 'თანხა'
-
-    let transactionInfoAmountText = document.createElement('p')
-    transactionInfoAmountText.setAttribute('id', 'transactionAmount')
-    // transactionInfoAmountText.textContent = transactionAmount + " " + transactionCurrency
-    transactionInfoAmountText.textContent = `${transactionAmount}  ${transactionCurrency}`
-
-    transactionInfoAmount.appendChild(transactionInfoAmountLabel)
-    transactionInfoAmount.appendChild(transactionInfoAmountText)
-
-    //append to UL
-    transactionBodyUl.appendChild(transactionTypeLabel)
-    transactionBodyUl.appendChild(transactionDate)
-    transactionBodyUl.appendChild(transactionAmountLabel)
-    transactionBodyUl.appendChild(transactionInfo)
-    transactionBodyUl.appendChild(transactionInfoAmount)
     
-
-    transactionBody.appendChild(transactionBodyUl)
-    transactionArticle.appendChild(transactionBody)
-    transactionWrapper.appendChild(transactionArticle)
+    transactionItemID.textContent = `Transaction #${transactionid}`
+    itemType.textContent = transactionType
+    itemAmount.textContent = `${determineType(transactionType, transactionAmount, "შემოსავალი")} ${transactionCurrency}`
+    itemDesc.textContent =  `${transactionType} / თანხა ${transactionCurrency} ${transactionAmount} / ${transactionDate}`
 
     transactionWrapper.style.display = "grid"
 
